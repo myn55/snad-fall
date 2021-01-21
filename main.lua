29b11a1332f265eaea99fe234370bb7b92feb5e8
Coronhaus <coronhaus@gmail.com>
2021-01-21 18:58:58 -0500

    Basic window settings Width 1200 Height 800 Scale 8

--- main.lua
+++ main.lua
@@ -3,13 +3,20 @@ local function setColor(r, g, b)
     love.graphics.setColor(r/255, g/255, b/255)
 end
 
---[[ Main ]]--
-function love.load()
 
+--[[ Main ]]--
+
+-- Constants
+local scale = 8
+local width, height = 1200, 800
+
+function love.load()
+    love.window.setTitle("snad fall")
+    love.window.setMode(width, height, {centered = true, resizable = false})
 end
 
 function love.update(dt)
-
+    
 end
 
 function love.draw()
