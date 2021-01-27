ee97821889c83947eec58470ad16dca09b560089
Coronhaus <coronhaus@gmail.com>
2021-01-26 19:14:41 -0500

    Made draw loop start from top down, update loop should be from bottom top

--- main.lua
+++ main.lua
@@ -86,7 +86,7 @@ function love.draw()
 
     -- TODO
     -- Draw cells
-    for j=grid.columns, 1, -1 do -- TODO make draw loop start from bottom up
+    for j=1, grid.columns do
         for i=1, grid.rows do
             local cell = grid.cellarray[i][j]
             local x, y = (i-1)*SCALE, (j-1)*SCALE
